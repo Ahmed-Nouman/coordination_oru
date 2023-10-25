6f952a7811e5c3fe16f87596202efc6002898036
Ahmed Nouman <ahmed.n1985@gmail.com>
2023-10-25 16:27:31 +0200

    Fixed build.gradle to run JavaFX in the project

---
+++ src/main/java/se/oru/coordination/coordination_oru/gui_JavaFX/GUI.java
@@ -0,0 +1,16 @@
+package se.oru.coordination.coordination_oru.gui_JavaFX;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class GUI extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+        System.out.println("Finally");
+    }
+}
