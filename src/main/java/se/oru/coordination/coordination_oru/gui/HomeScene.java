5367437c741d1ceec6664f532eeb651ed8d70acb
Ahmed Nouman <ahmed.n1985@gmail.com>
2024-01-05 00:03:00 +0100

    NavigationBar class has been updated

---
+++ src/main/java/se/oru/coordination/coordination_oru/gui/HomeScene.java
@@ -0,0 +1,4 @@
+package se.oru.coordination.coordination_oru.gui;
+
+public class HomeScene {
+}
---
+++ src/main/java/se/oru/coordination/coordination_oru/gui/HomeSceneController.java
@@ -0,0 +1,10 @@
+package se.oru.coordination.coordination_oru.gui;
+
+public class ProjectSceneController implements SceneController {
+    // ... other methods specific to the Home scene ...
+
+    @Override
+    public void setupScene() {
+        // Setup code specific to the Home scene
+    }
+}
